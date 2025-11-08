a2eca42a8830c15ab7c8a753a554ccffc71505da
Vaibhav <vaibhav0479.becse24@chitkara.edu.in>
2025-11-08 22:00:53 +0530

    Time: 0 ms (100%), Space: 42.1 MB (13%) - LeetHub

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/1732-minimum-one-bit-operations-to-make-integers-zero.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumOneBitOperations(int n) {
+        int ans = 0;
+
+        while (n > 0) {
+            ans = ans ^ n;
+            n = n >> 1;
+        }
+
+        return ans;
+    }
+}
